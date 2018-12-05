a728680ffe1a0bc9245a86ce3c6850f9ece020a0
Hodaya Marciano <hodayamar2000@gmail.com>
2018-12-05 12:40:43 -0800

    basic smart_ptr API

--- s_ptr.h
+++ s_ptr.h
@@ -1,8 +1,46 @@
-//
-// Created by hodaya on 12/5/18.
-//
-
 #ifndef EXCELLENTEAM_ELLA_CPP_SMART_POINTERS_HODAYAMAR_S_PTR_H
 #define EXCELLENTEAM_ELLA_CPP_SMART_POINTERS_HODAYAMAR_S_PTR_H
+
+template<typename T>
+class UniquePtr
+{
+public:
+    explicit UniquePtr(T* ptr );
+    ~UniquePtr();
+
+    T* operator->() const;
+    T& operator*()  const;
+
+    T* get() const;
+
+private:
+    // Prevent coping
+    UniquePtr(UniquePtr const&);
+    UniquePtr& operator=(UniquePtr const&);
+
+    T*   m_ptr;
+};
+
+template<typename T>
+UniquePtr<T>::UniquePtr(T* ptr ):m_ptr(ptr)
+{}
+
+template<typename T>
+UniquePtr<T>::~UniquePtr()
+{
+    delete m_ptr;
+}
+
+template<typename T>
+T & UniquePtr<T>::operator* ()const
+{
+    return *m_ptr;
+}
+
+template<typename T>
+T * UniquePtr<T>::operator-> ()const
+{
+    return m_ptr;
+}
 
 #endif //EXCELLENTEAM_ELLA_CPP_SMART_POINTERS_HODAYAMAR_S_PTR_H
