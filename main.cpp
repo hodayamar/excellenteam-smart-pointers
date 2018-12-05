f4e5f814a3bfa3b9dad957ff8d81c694ac7c53e5
Hodaya Marciano <hodayamar2000@gmail.com>
2018-12-05 15:09:44 -0800

    overide bool operator of smart_ptr

--- main.cpp
+++ main.cpp
@@ -1,4 +1,39 @@
-//
-// Created by hodaya on 12/5/18.
-//
+#include "s_ptr.h"
+#include <cstring>//strcpy
+
+class Person {
+
+public:
+
+    Person();
+    Person(const char * full_name);
+
+    void print()const;
+
+private:
+
+    char m_full_name[32];
+
+};
+
+
+int main(){
+
+    UniquePtr<Person> p(new Person("Shlomit"));
+
+    if (p)
+        
+        p -> print();
+
+    return 0;
+}
+
+Person::Person(const char * full_name)
+{
+    strcpy(m_full_name, full_name);
+}
+void Person::print()const
+{
+    std::cout << "m_full_name: " << m_full_name << std::endl;
+}
 
