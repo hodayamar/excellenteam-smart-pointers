0ce041f2c40f72dd9ee1988865557584753e41b3
Hodaya Marciano <hodayamar2000@gmail.com>
2018-12-05 15:08:08 -0800

    Prevent assignment operator from T

--- s_ptr.h
+++ s_ptr.h
@@ -1,15 +1,22 @@
 #ifndef EXCELLENTEAM_ELLA_CPP_SMART_POINTERS_HODAYAMAR_S_PTR_H
 #define EXCELLENTEAM_ELLA_CPP_SMART_POINTERS_HODAYAMAR_S_PTR_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <iostream>//cout
+
 template<typename T>
 class UniquePtr
 {
 public:
-    explicit UniquePtr(T* ptr );
+
+    explicit UniquePtr(T* ptr = NULL);
     ~UniquePtr();
 
     T* operator->() const;
     T& operator*()  const;
+    T& operator = (T*);
+    operator bool();
 
     T* get() const;
 
@@ -23,7 +30,11 @@ private:
 
 template<typename T>
 UniquePtr<T>::UniquePtr(T* ptr ):m_ptr(ptr)
-{}
+{
+
+//    if (!ptr)
+//        throw std::invalid_argument();
+}
 
 template<typename T>
 UniquePtr<T>::~UniquePtr()
@@ -43,4 +54,16 @@ T * UniquePtr<T>::operator-> ()const
     return m_ptr;
 }
 
+template<typename T>
+T* UniquePtr<T>::get() const
+{
+    return m_ptr;
+}
+
+template<typename T>
+UniquePtr<T>::operator bool()
+{
+    return m_ptr;
+}
 #endif //EXCELLENTEAM_ELLA_CPP_SMART_POINTERS_HODAYAMAR_S_PTR_H
+
