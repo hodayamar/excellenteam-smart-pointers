bb9e77ad2dac2809730a89b2cfdb4140e515c1ce
Hodaya Marciano <hodayamar2000@gmail.com>
2018-12-05 22:54:22 -0800

    Adding tests

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ public:
     Person(const char * full_name);
 
     void print()const;
+    const char * get_full_name()const;
 
 private:
 
@@ -17,13 +18,53 @@ private:
 };
 
 
-int main(){
+void person_to_s_ptr()
+{
+    char * first_ptr = new char[1];
 
+    {
+        UniquePtr<Person> p(new Person("Shlomit"));
+    }
+
+    char * second_ptr = new char[1];
+
+    if(&second_ptr - &first_ptr == 1)
+
+        std::cout << "smart_ptr is smart!" << std::endl;
+}
+
+void arrow_test()
+{
+    UniquePtr<Person> p(new Person("Shlomit"));
+
+    if (strcmp(p->get_full_name(), "Shlomit") == 0)
+        std::cout << "arrow is OK" << std::endl;
+}
+
+void test_bool()
+{
     UniquePtr<Person> p(new Person("Shlomit"));
 
     if (p)
-        
-        p -> print();
+        std::cout << "Bool is OK" << std::endl;;
+
+}
+
+void test_star()
+{
+    UniquePtr<Person> p(new Person("Shlomit"));
+
+    if (strcmp((*p).get_full_name(), "Shlomit") == 0)
+        std::cout << "star is OK" << std::endl;
+}
+
+int main(){
+
+    person_to_s_ptr();
+    arrow_test();
+    test_bool();
+    test_star();
+
 
     return 0;
 }
@@ -32,8 +73,15 @@ Person::Person(const char * full_name)
 {
     strcpy(m_full_name, full_name);
 }
+
 void Person::print()const
 {
     std::cout << "m_full_name: " << m_full_name << std::endl;
 }
+
+const char * Person::get_full_name()const
+{
+    return m_full_name;
+}
+
 
